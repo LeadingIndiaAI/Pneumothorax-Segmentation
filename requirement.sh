4da37bfb5d5b73a96eed9bc8961a511b9b10e8b4
royal1456 <36342283+royal1456@users.noreply.github.com>
2019-07-16 15:17:11 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,5 +1,3 @@
 pip install --upgrade pip
 pip install pydicom tqdm jupyter scikit-learn
-apt update && apt install -y libsm6 libxext6 libxrender-dev git unzip 
 pip install keras matplotlib image pandas opencv-python opencv-contrib-python imgaug h5py
-apt-get install libxrender1
